b7119240cf9b3a909852a05771371130de7fc35a
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-18 15:08:25 +0100

    Aded sample unit test ptoject with 1 sample unit test.

---
+++ tests/Dfc.ProviderPortal.Lars.LearningDelivery.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dfc.ProviderPortal.Lars.LearningDelivery.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
